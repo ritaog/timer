8f333a3963acfc675cdb3a5fadf630f2c6a7493c
Rita Ogedegbe <ritaogedegbe@yahoo.co.uk>
2021-12-12 23:36:26 -0700

    Implements timer functionality

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+  const [time, setTime] = useState(new Date().toLocaleTimeString());
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  useEffect(() => {
+    setTimeout(() => {
+      setTime(new Date().toLocaleTimeString());
+    }, 1000);
+  }, [time]);
+
+  return <div>The time is: {time}</div>;
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
